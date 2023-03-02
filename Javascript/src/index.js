9116563827d30e7799cb36b8aa7c82a7b0299b43
Shubham Khera <shubham_khera@hotmail.com>
2023-03-02 20:52:48 +0530

    JavaScript commit

---
+++ Javascript/src/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    var n = "Test"
+
+    function printName(){
+        var b = 'tile'
+        console.log("Hello" + n + b)
+
+    }
+
+    printName()
+    console.log(b)
+
+})
